945793aed1f717a813e3bd01f780b3dfd64f33df
mhkane <mhkane@mit.edu>
2014-10-04 15:30:23 -0400

    Parse added to iOS project

---
+++ badmouth/badmouth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  badmouth
+//
+//  Created by Mohamed Kane on 10/4/14.
+//  Copyright (c) 2014 Mohamed Kane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
